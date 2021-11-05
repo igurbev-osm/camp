1c02aee9209f2d185b92988bd04ed28da7e2101e
igurbev <igurbev@gmail.com>
2021-11-05 10:51:53 +0200

    icon, out of the map handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import MapComponent from './components/MapComponent';
 
 
-const DefaultLocation = { lat: 42.69140841618888, lng: 23.99100942655395 };
-const DefaultZoom = 10;
-
 const App = () => {
 
-  return <MapComponent />
+  return <MapComponent 
+    onMapClick={e => {
+        console.log(`{lat: ${e.latLng.lat()}, lng: ${e.latLng.lng()}}`);
+        alert("Selected point: " + `{lat: ${e.latLng.lat()}, lng: ${e.latLng.lng()}}`);
+      }
+    }
+    onMarkerClick = {e => {
+      console.log(`{lat: ${e.latLng.lat()}, lng: ${e.latLng.lng()}}`);
+      alert(`MarkerClicked: ${e.domEvent.currentTarget.title} | {lat: ${e.latLng.lat()}, lng: ${e.latLng.lng()}}`);
+    }}
+  />
 }
 
 export default App
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -4,26 +4,20 @@ import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import { mapConfig } from '../config/config';
 
 
-let marks = [];
-
-//create your forceUpdate hook
-function useForceUpdate() {
-  let [value, setState] = useState(true);
-  return () => setState(!value);
-}
-
-function MapComponent() {
-  const forceUpdate = useForceUpdate();
+function MapComponent(props) {
+  
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyD07E1VvpsN_0FvsmKAj4nK9GnLq-9jtj8"
   });
 
-  const [map, setMap] = useState(null)
+  const [markers, setMarkers] = useState( mapConfig.initialMarkers);
+  const [map, setMap] = useState(null);
 
   const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
+    // const bounds = new window.google.maps.LatLngBounds();
+    // map.fitBounds(bounds);
+    
     setMap(map);
   }, [])
 
@@ -31,28 +25,22 @@ function MapComponent() {
     setMap(null);
   }, [])
 
-
-  function mapClick(e) {
-    marks.push({ lat: e.latLng.lat(), lng: e.latLng.lng() });    
-    console.log("click: ", e.latLng.lat(), e.latLng.lng());
-    forceUpdate();
-  }
-
-  function markerClick(e){
-    console.log("marker click: ", e.latLng.lat(), e.latLng.lng());
+  const onMapClick = (e)=>{
+    setMarkers(markers.concat([{lat: e.latLng.lat(), lng: e.latLng.lng()}]));
+    props.onMapClick(e);
   }
 
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={mapConfig.size}
       center={mapConfig.center}
-      zoom={9}
+      zoom={mapConfig.zoom}
       onLoad={onLoad}
       onUnmount={onUnmount}
-      onClick={mapClick}
+      onClick={onMapClick}
     >
 
-      {marks.map((mark, index) => <Marker onClick={markerClick} key={index} position={mark} />)}
+      {markers.map((mark, index) => <Marker onClick={props.onMarkerClick} key={index} position={mark} title={"marker " + index} icon={{url: "/img/tourism_camp_site.png"}}/>)}
 
     </GoogleMap>
   ) : <></>
