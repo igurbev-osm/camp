b6446aff3f023f5d01b1cb92329cd5212dffa8f7
igurbev <igurbev@gmail.com>
2021-11-05 13:18:38 +0200

    parent - child communication

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, memo, forwardRef, useImperativeHandle } from 'react'
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 
 import { mapConfig } from '../config/config';
 
 
-function MapComponent(props) {
+function MapComponent(props, ref) {
   
+  useImperativeHandle(
+    ref,
+    () => ({
+        addMarkers(newMarkers, isAdd) {
+          let markersArr = isAdd ? markers.concat(newMarkers) : [].concat(newMarkers);
+          setMarkers(markersArr);
+        }
+    }),
+  )
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyD07E1VvpsN_0FvsmKAj4nK9GnLq-9jtj8"
@@ -25,10 +35,6 @@ function MapComponent(props) {
     setMap(null);
   }, [])
 
-  const onMapClick = (e)=>{
-    setMarkers(markers.concat([{lat: e.latLng.lat(), lng: e.latLng.lng()}]));
-    props.onMapClick(e);
-  }
 
   return isLoaded ? (
     <GoogleMap
@@ -37,7 +43,7 @@ function MapComponent(props) {
       zoom={mapConfig.zoom}
       onLoad={onLoad}
       onUnmount={onUnmount}
-      onClick={onMapClick}
+      onClick={props.onMapClick}
     >
 
       {markers.map((mark, index) => <Marker onClick={props.onMarkerClick} key={index} position={mark} title={"marker " + index} icon={{url: "/img/tourism_camp_site.png"}}/>)}
@@ -46,4 +52,4 @@ function MapComponent(props) {
   ) : <></>
 }
 
-export default React.memo(MapComponent)
+export default MapComponent;
