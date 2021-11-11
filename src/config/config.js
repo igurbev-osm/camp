a9ea29faf9eda63865be0c077a6387f5ca1f459b
Nevena Georgieva <nevena.georgieva@siemens.com>
2021-11-11 19:55:02 +0200

    logo and navbar sass

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 export const mapConfig = {
         zoom: 8,
-        size: {width: '900px', height: '600px' },
+        // size: {width: '1900px', height: '1200px'},
+        // size: {width: '900px', height: '600px' },
         center: {lat: 42.645234602964514, lng: 25.017259689118312}
 }
 
