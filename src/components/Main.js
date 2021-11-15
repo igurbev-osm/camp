b6f931a3499a2fe7ffe79931e4b5aebeef3fa95f
igurbev <igurbev@gmail.com>
2021-11-15 22:24:18 +0200

    refactoring

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,71 +1,28 @@
-import React, {useRef, forwardRef, useState, useEffect} from "react";
+import React, {useState, useEffect } from "react";
 import _pointService from "../server/point";
 import MapComponent from "./MapComponent";
-import AddPointPopup from './popups/AddPointPopup';
-import { useSelector, useDispatch } from 'react-redux';
-import initUserManager from "../utils/userManager";
 
-function Main(){    
-        const userManager = initUserManager(useSelector, useDispatch);
-        const childRef = useRef();
-        const Map = forwardRef(MapComponent);
-        const [modalShow, setModalShow] = useState(false); 
-        const [currentSelection, setSelection] = useState(null);
-        const [pointTypes, setPointTypes] = useState(null);
+function Main() {
+  
+  const [pointTypes, setPointTypes] = useState(null);
+  useEffect(() => {
+    (
+      async () => {
+        setPointTypes(await _pointService.getPointTypes());
+      }
+    )();
+  }, []);
 
-        useEffect(() => {
-          (
-            async()=>{
-              setPointTypes(await _pointService.getPointTypes());
-            }
-          )();
-        }, []);
-       
-        return <><Map 
-          onMapClick={
-              e => {
-                if(userManager.sid){
-                // const pointName = prompt("Enter point name");
-                // if(pointName && pointName !== ""){
-                // let point = {title: pointName, lat: e.latLng.lat(), lng: e.latLng.lng(), iconid: 1, description: "Wild camp"};
-                // if(userManager.sid){
-                // (
-                //     async()=>{
-                //         point = await _pointService.addPoint(userManager.sid, point);
-                //         childRef.current.addMarkers([
-                //             point
-                //           ], true);
-                //     }
-                // )();
-                // }
-               
-              //}
-                setSelection({lat: e.latLng.lat(), lng: e.latLng.lng()});
-                setModalShow(true);
-              }
-              console.log(`{lat: ${e.latLng.lat()}, lng: ${e.latLng.lng()}}`);              
-              
-            }
-          }
-          onMarkerClick = {e => {              
-            console.log(`Marker: ${e.domEvent.currentTarget.title} | {lat: ${e.latLng.lat()}, lng: ${e.latLng.lng()}}`);           
-          }}
-          ref={childRef}
-        />
-         <AddPointPopup
-            show={modalShow}
-            onHide={(point) => {
-              if(point){
-                childRef.current.addMarkers([point], true);
-              }
-              setModalShow(false);
-            }}
-            selection={currentSelection}
-            pointtypes={pointTypes}
-            sid={userManager.sid}
-          />
-        </>
-    
+  return <> {
+    pointTypes &&
+    <MapComponent
+      pointTypes={pointTypes}
+      onMarkerClick={e => {
+        console.log(`Marker: ${e.domEvent.currentTarget.title} | {lat: ${e.latLng.lat()}, lng: ${e.latLng.lng()}}`);
+      }}
+    />}
+  </>
+
 }
 
 export default Main;
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,67 +1,81 @@
-import React, { useState, useImperativeHandle } from 'react'
+import React, { useState } from 'react'
 import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api';
 import _pointService from "../server/point";
 import { useSelector, useDispatch } from 'react-redux';
 import initUserManager from "../utils/userManager";
-import {reloadConf} from "../utils/reloadConfig"
+import { reloadConf } from "../utils/reloadConfig"
 
 import { mapConfig } from '../config/config';
+import AddPointPopup from './popups/AddPointPopup';
 
-function MapComponent(props, ref) {
+function MapComponent(props) {
   const userManager = initUserManager(useSelector, useDispatch);
   const [map, setMap] = useState(null);
-  const [markers, setMarkers] = useState( []);
-  useImperativeHandle(
-    ref,
-    () => ({
-        addMarkers(newMarkers, isAdd) {
-          let markersArr = isAdd ? markers.concat(newMarkers) : [].concat(newMarkers);
-          setMarkers(markersArr);
-        }
-    }),
-  )
+  const [markers, setMarkers] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
+  const [currentSelection, setSelection] = useState(null);
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyD07E1VvpsN_0FvsmKAj4nK9GnLq-9jtj8"
   });
-  
-  return isLoaded ? (
-    <GoogleMap      
-      mapContainerStyle={{width: 'calc(100%)', height: '600px' }}
-      center={mapConfig.center}
-      zoom={mapConfig.zoom}     
-      onClick={props.onMapClick}
-      onBoundsChanged={ () => {
-        (
-          async ()=>{                       
-            reloadConf.currentBounds =  map.getBounds();
-            if(!reloadConf.isRequestStart){
-              reloadConf.isRequestStart = true;
-              setTimeout(async () => {  
-                setMarkers(await _pointService.getPoints(0, reloadConf.currentBounds));
-                reloadConf.isRequestStart = false;
-                reloadConf.reloadInterval = reloadConf.nextStepInterval;
-              }, reloadConf.reloadInterval);              
-            }
-            
+
+
+  return (<> {isLoaded && <GoogleMap
+    mapContainerStyle={{ width: 'calc(100%)', height: '600px' }}
+    center={mapConfig.center}
+    zoom={mapConfig.zoom}
+    onClick={e => {
+      if (userManager.sid) {
+        setSelection({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+        setModalShow(true);
+      }
+    }}
+    onBoundsChanged={() => {
+      (
+        async () => {
+
+          if (!reloadConf.isRequestStart) {
+            reloadConf.currentBounds = map.getBounds();
+            reloadConf.isRequestStart = true;
+            setTimeout(async () => {
+              setMarkers(await _pointService.getPoints(0, reloadConf.currentBounds));
+
+              reloadConf.isRequestStart = false;
+              reloadConf.reloadInterval = reloadConf.nextStepInterval;
+            }, reloadConf.reloadInterval);
           }
-        )();
-      }}      
-      onLoad= {async mapRef => {        
-        setMap(mapRef);        
+
+        }
+      )();
+    }}
+    onLoad={async mapRef => {
+      setMap(mapRef);
+    }}
+  >
+
+    {markers.map((mark, index) => <Marker
+      onClick={props.onMarkerClick}
+      key={index}
+      position={{ lat: mark.lat, lng: mark.lng }}
+      title={mark.title}
+      icon={mark.url ? { url: mark.url } : { url: "/img/tourism_camp_site.png" }} />)}
+
+  </GoogleMap>
+  }
+    <AddPointPopup
+      show={modalShow}
+      onHide={(point) => {
+        if (point) {
+          setMarkers(markers.concat([point]));
+        }
+        setModalShow(false);
       }}
-    >
-
-      {markers.map((mark, index) => <Marker
-       onClick={props.onMarkerClick} 
-       key={index} 
-       position={{lat: mark.lat, lng: mark.lng}} 
-       title={mark.title} 
-       icon={mark.url ? {url: mark.url} : {url: "/img/tourism_camp_site.png"}}/>)}
-
-    </GoogleMap>
-  ) : <></>
+      selection={currentSelection}
+      pointtypes={props.pointTypes}
+      sid={userManager.sid}
+    />
+  </>)
 }
 
 export default MapComponent;
