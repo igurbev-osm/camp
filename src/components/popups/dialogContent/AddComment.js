aaca18c5d37bf28b318f0ed9e6ec7aac54ee62be
igurbev <igurbev@gmail.com>
2021-12-08 20:12:27 +0200

    css fixes

--- src/components/popups/dialogContent/AddComment.js
+++ src/components/popups/dialogContent/AddComment.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Col, Container, Form, Image, Row, Button } from "react-bootstrap";
 import { SessionContext } from "../../../context/SessionContext";
 import _pointServiceF from "./../../../server/point";
@@ -6,36 +6,36 @@ import _pointServiceF from "./../../../server/point";
 const AddComment = ({ point, addStack, done }) => {
 
     const _axios = useContext(SessionContext);
-   const _pointService = _pointServiceF.bind(_axios)();
-    const onSubmit = (e) =>{
-        e.preventDefault();        
+    const _pointService = _pointServiceF.bind(_axios)();
+    const [error, setError] = useState(false);
+    const onSubmit = (e) => {
+        e.preventDefault();
         const form = e.target;
         const formData = new FormData(form);
         const comment = formData.get("comment");
 
-        if(isValid(comment)){
+        if (isValid(comment)) {
             postComment(comment);
-        }else{
+        } else {
             console.log("invalid comment: ", comment);
             //TODO: show invalid comment message
         }
     };
 
-    const postComment = async (comment)=>{
+    const postComment = async (comment) => {
         await _pointService.commentPoint(point.id, comment);
         point.ccount++;
         done(point);
     }
 
-    const isValid = (comment) =>{
+    const isValid = (comment) => {
         return comment && comment.length > 20;
     }
 
     return (
         <div className="dialog-content-holder">
             <Form onSubmit={onSubmit} className="add-point-form">
-                <Container>            
-
+                <Container>
                     <Row>
                         <Col>
                             <Form.Group md="4" controlId="validationCustom02" >
@@ -45,9 +45,14 @@ const AddComment = ({ point, addStack, done }) => {
                                     required
                                     placeholder="Add your comment here."
                                     style={{ height: '320px' }}
+                                    isInvalid={error}
+                                    onChange={e => {
+                                        const newValue = !(e.currentTarget.value.length > 19);
+                                        setError(error => error === newValue ? error : newValue);
+                                    }}
                                 />
-                                <Form.Control.Feedback type="invalid">
-                                    Add your comment here.
+                                <Form.Control.Feedback type="invalid" className="validation-feedback">
+                                    The comment must be at least 20 characters long.
                                 </Form.Control.Feedback>
                             </Form.Group>
                         </Col>
@@ -59,11 +64,10 @@ const AddComment = ({ point, addStack, done }) => {
                             </div>
                         </Col>
                     </Row>
-                   
-
-                    <Image src="/img/CamPointLogo.png" className="add-point-logo" />
-
                 </Container>
+                <div className="add-point-logo-holder">
+                    <Image src="/img/CamPointLogo.png" className="add-point-logo" />
+                </div>
                 <div className="content-footer">
                     <Button variant="primary" type="submit" className="next-button" > Add </Button>
                     <Button variant="primary" className="btn-secondary" onClick={() => done(point)} > Close </Button>
